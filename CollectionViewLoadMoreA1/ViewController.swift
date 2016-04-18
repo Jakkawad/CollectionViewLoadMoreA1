c7fc768e8e10986916d2008c69249a2f38fbb9ad
Jakkawad <admin@all2sale-imac.local>
2016-04-18 10:14:30 +0700

    basic

--- CollectionViewLoadMoreA1/ViewController.swift
+++ CollectionViewLoadMoreA1/ViewController.swift
@@ -8,8 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    private var numberOfItemPerSection = 12
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numberOfItemPerSection
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let col0 = collectionView.dequeueReusableCellWithReuseIdentifier("collectCell0", forIndexPath: indexPath)
+        
+        return col0
+    }
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        
+        if offsetY > contentHeight - scrollView.frame.size.height {
+            numberOfItemPerSection += 6
+            self.collectionView.reloadData()
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
